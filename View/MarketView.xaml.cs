faf52d7adb98457c794e7970f302c3c9509ac54a
IrynaNakoneshniuk <103773315+IrynaNakoneshniuk@users.noreply.github.com>
2023-01-31 21:41:07 +0200

    Add main window

---
+++ View/MarketView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection.Emit;
+using System.Windows.Controls;
+using LiveCharts;
+using LiveCharts.Wpf;
+namespace Crypto_Info.View
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        private string[] _labels;
+        public UserControl1()
+        {
+            InitializeComponent();
+
+
+            _labels = new string[] { "10", "40", "60", "70" };
+        }
+       
+    }
+}
